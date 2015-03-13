87fdd5b44fab753934754d078ecf83557544501e
deathcap <deathcap@gmx.co.uk>
2015-03-12 20:53:26 -0700

    Translate module.exports= to 'export default' declaration

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # cjs2es6export
 
-Convert a subset of CommonJS/NodeJS [module](https://nodejs.org/api/modules.html#modules_modules) assignments to
-[ECMAScript 6 module export](http://www.2ality.com/2014/09/es6-modules-final.html) statements.
+Convert a subset of CommonJS/NodeJS [module.exports](https://nodejs.org/api/modules.html#modules_modules) assignments to
+[ECMAScript 6 module export](http://www.2ality.com/2014/09/es6-modules-final.html) declarations.
+
+Usage:
+
+    var cjs2es6export = require('cjs2es6export');
+
+    var src = "module.exports = function() { return 42; };"
+    var newSrc = cjs2es6export(src); // "export default function() {\n return 42;\n};;"
+
+Currently only direct assignment to `module.exports` (translated to `default export`) is supported.
+
+See also [cjs2es6import](https://github.com/deathcap/cjs2es6import)
 
 ## License
 
--- cjs2es6export.js
+++ cjs2es6export.js
@@ -26,78 +26,25 @@ module.exports = function(src, opts) {
     return node.left.type === 'MemberExpression' &&
       node.left.object.type === 'Identifier' && node.left.object.name === 'module' &&
       node.left.property.type === 'Identifier' && node.left.property.name === 'exports';
+    // TODO: detect module.exports.foo = (or exports.foo =) property assignments, non-default exports
   };
 
   walk.simple(ast, walkall.makeVisitors(function(anode) {
     if (!isExportAssignment(anode))
       return;
 
-    console.log("ANODE",anode);
-
     if (anode.right.type === 'FunctionExpression') {
       var f = anode.right;
       object_clear(anode);
-      object_assign(anode, f);
-      console.log("NEW",anode);
+
+      // export default function()
+      anode.type = 'ExportDeclaration';
+      anode.default = true;
+      anode.declaration = f;
+
     }
 
-
-    //console.log('Found node type',anode.type,anode);
-    //console.log(escodegen.generate(node));
-
-    if (anode.type !== 'VariableDeclaration') {
-      return;
-    }
-
-    var newNodes = [];
-
-    anode.declarations.forEach(function(node) {
-      if (node.id.type !== 'Identifier') {
-        //console.log('Ignoring non-identifier variable identifier: ',node);
-        return;
-      }
-
-      var varName = node.id.name;
-
-      if (isRequire(node.init)) {
-        if (node.init.arguments.length) {
-          if (node.init.arguments[0].type === 'Literal') {
-            var moduleName = node.init.arguments[0].value;
-
-            //console.log('Found require:', varName, moduleName);
-            //console.log('Old node=',node);
-
-            delete node.id;
-            delete node.init;
-            node.type =  'ImportDeclaration';
-            node.source = {type: 'Literal', value: opts.encode(moduleName)}
-
-            node.specifiers = [
-              {
-                type: 'ImportDefaultSpecifier',
-                id: {type: 'Identifier', name: varName},
-              }
-            ];
-
-            newNodes.push(node);
-
-            //console.log('New node=',node);
-          } else {
-            //console.log('Ignored non-string require:',node.init.arguments[0]);
-            // Pass it through unchanged, not much we can do TODO: support subset of expression evaluations like browserify
-            newNodes.push(node);
-          }
-        }
-      }
-    });
-
-    anode.type = 'Program';
-    anode.body = newNodes;
-    delete anode.kind;
-    delete anode.declarations;
     //console.log('anode=',anode);
-    //
-
   }), walkall.traversers);
 
   var newSrc = escodegen.generate(ast);
